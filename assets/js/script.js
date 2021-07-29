ccd8ea92c6b0b32179d4193fb22139ae0789c8bd
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-07-29 01:15:44 -0400

    20210729 Finally solved the mystery of the non-saving element. Buggy behaviour with array not being recognized, despite global.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,35 @@
 //establish a root element to attach dynamically generated pieces
+//Strangest variable weirdness ever
+var trackedTasks = [];
+
 var timeBlockEl = $('#timeSchedule');
 var currentDayEl = $('#currentDay');
 var rightNow = moment().format('MMM DD, YYYY [at] hh:mm a');
 //object to hold and render activities
-var taskTracker = [
-    {
-        hour: "",
-        activity: "",
-    }
-];
-var workTime = 16;
-var merdian = 'am';
-var startTime = 7;
+debugger
+
+let workTime = 16;
+let merdian = 'am';
+let startTime = 7;
 
 function SaveActivity (clickedButton){
-    debugger
-    console.log(clickedButton);
+
     //Naming convention is to have the index slice of and give the id name of the text field to save
-    x.toLocaleLowerCase() = clickedButton.slice(clickedButton.indexOf('-'))
+    let x = "";
+    let y = "";
+    //always slice after the known '-' existence spot
+    x = clickedButton.slice(clickedButton.indexOf('-')+1)
+    y = clickedButton.match(/\d+/)[0];
+    z = $('#'+x.toLocaleLowerCase()).val();
+
     //Take jquery of object by index value
-    if($('#'+x).attr('class', 'past')){
-        alert('The hour has already past. No text will be retained')
+    if(z !== ""){
+        var localTaskTemp ={
+            activity: z,
+            hour: $('#time'+y)[0].innerHTML
+        };
+        trackedTasks.push(localTaskTemp);
+        localStorage.setItem("taskTracker", JSON.stringify(trackedTasks));
     }
 }
 /**?
@@ -53,7 +62,8 @@ function colorCodeWorkHours(){
     for(var j = (present - 1); j >= 0; j--){
         $('#activity'+j).addClass('past');
         //using jquery to grab element and change the inner text
-        $('#activity'+j).text("The time has passed.");
+        // $('#activity'+j).text("The time has passed.").prop('disabled', true);
+        // $('#save-Activity'+j).prop('disabled', true);
     }
     //assumes all cases going forward, but would miss 0 index while looping
     //checks if 0, if true paint all
@@ -80,7 +90,14 @@ function init(){
         startTime++;
     };
 
-
+    var taskReturner = JSON.parse(localStorage.getItem("taskTracker"))
+    debugger;
+    
+    //check there are items in local storage
+    if(taskReturner !== null){
+        trackedTasks = taskReturner;
+    }
+    setActivities();
 }
 //Register what button was clicked. Don't react to anything but button clicks
 //if a button, find out which one and go to save event
@@ -94,6 +111,20 @@ timeBlockEl.click("click", function(event) {
 
 function displayTime() {
     currentDayEl.text(rightNow);
+}
+
+function setActivities(){
+   if(trackedTasks.length > 0 && trackedTasks !== null){
+    //cycle through local stored items
+        for(var j = 0; j < trackedTasks.length; j++){
+            //cycle through activities till times match, then use that 
+            for(var i = 0; i < workTime; i++){
+                if($('#time'+i)[0].innerHTML == trackedTasks[j].hour){
+                    $('#activity'+i).val(trackedTasks[j].activity);
+                }
+            }
+        }
+    }
 }
 
 //Functions that require running on page load
